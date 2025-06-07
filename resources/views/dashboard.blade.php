b1b407b73ad1377debcc0105c560195a36a367c0
nombrePrueba <email@email.com>
2025-06-07 15:40:05 +0200

    cambio x

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -104,9 +104,6 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">Borrar Usuario</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
         </div>
         <div class="modal-body">
          Al borrar un usuario todos sus datos serán borrados (pedidos y datos personales) y irrecuperables. 
